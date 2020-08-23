f48bcadcd2417a50e8170c71e256b46ad7602dd5
fan-0v0 <471174298@qq.com>
2020-08-23 13:46:31 +0800

    week9

---
+++ Week_09/最长回文子串.go
@@ -0,0 +1,24 @@
+func longestPalindrome(s string) string {
+	if s == "" {
+		return s
+	}
+    for length := len(s); length > 1; length-- {
+		for i := 0; i + length <= len(s); i++ {
+			if isPal(s, i, i + length - 1) {
+				runes := []rune(s)
+				ans := string(runes[i:i+length])
+				return ans
+			}
+		}
+	}
+	return string(s[0])
+}
+
+func isPal(s string, start int, end int) bool {
+	for i, j := start, end; i < j; i, j = i + 1, j - 1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
+}
